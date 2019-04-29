63b1d66d85ea21464af980d073cf07ea6b2e5f3c
Waga Odongo <wagaodongo@gmail.com>
2019-04-29 14:35:40 +0300

    auth controller
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,28 @@
-exports.validateSignup = () => {};
+exports.validateSignup = (req, res, next) => {
+	req.sanitizeBody("name");
+	req.sanitizeBody("email");
+	req.sanitizeBody("password");
+
+	//Name is non-null and is 4 to 10 characters long
+	req.checkBody("name", "Enter A Name").notEmpty();
+	req
+		.checkBody("name", "Name must be between 4 and 10 characters")
+		.isLength({ min: 4, max: 10 });
+
+	//Email is non-null, valid and normalized
+
+	req
+		.checkBody("email", "Enter a valid email")
+		.isEmail()
+		.normalizeEmail();
+
+	//Password is non-null and is 4 to 10 characters long
+
+	req.checkBody("name", "Enter A Name").notEmpty();
+	req
+		.checkBody("name", "Name must be between 4 and 10 characters")
+		.isLength({ min: 4, max: 10 });
+};
 
 exports.signup = () => {};
 

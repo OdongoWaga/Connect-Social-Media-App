07830d3c8a337881b5df1e426687e24ee14b5f4a
Waga Odongo <wagaodongo@gmail.com>
2019-05-06 15:06:20 +0300

    comments
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- components/index/Comments.js
+++ components/index/Comments.js
@@ -1,16 +1,97 @@
-// import CardHeader from "@material-ui/core/CardHeader";
-// import FormControl from "@material-ui/core/FormControl";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Input from "@material-ui/core/Input";
-// import Avatar from "@material-ui/core/Avatar";
-// import Delete from "@material-ui/icons/Delete";
+import CardHeader from "@material-ui/core/CardHeader";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import Avatar from "@material-ui/core/Avatar";
+import Delete from "@material-ui/icons/Delete";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "next/link";
 
 class Comments extends React.Component {
-	state = {};
+	state = {
+		text: ""
+	};
+
+	handleChange = (e) => {
+		this.setState({ text: e.target.value });
+	};
+
+	handleSubmit = (e) => {
+		const { text } = this.state;
+		const { postId, handleAddComment } = this.props;
+
+		e.preventDefault();
+
+		handleAddComment(postId, text);
+		this.setState({ text: "" });
+	};
+	showComment = (comment) => {
+		const { postId, auth, classes } = this.props;
+		const { text } = this.state;
+		const isCommentCreator = comment.postedBy._id === auth.user._id;
+
+		return (
+			<div>
+				<Link href={`/profile/${comment.postedBy._id}`}>
+					<a> {comment.postedBy.name} </a>
+				</Link>
+				<br />
+				{comment.text}
+				<span className={classes.commentDate}>
+					{comment.createdAt}
+					{isCommentCreator && (
+						<Delete color="secondary" className={classes.commentDelete} />
+					)}
+				</span>
+			</div>
+		);
+	};
 
 	render() {
-		return <div>Comments</div>;
+		const { auth, comments, classes } = this.props;
+		const { text } = this.state;
+
+		return (
+			<div className={classes.comments}>
+				{/* Comment Input */}
+				<CardHeader
+					avatar={
+						<Avatar className={classes.smallAvatar} src={auth.user.avatar} />
+					}
+					title={
+						<form onSubmit={this.handleSubmit}>
+							<FormControl margin="normal" required fullWidth>
+								<InputLabel htmlFor="add-comment"> Add Comment </InputLabel>
+								<Input
+									id="add-comment"
+									name="text"
+									placeholder="Reply To this Post"
+									value={text}
+									onChange={this.handleChange}
+								/>
+							</FormControl>
+						</form>
+					}
+					className={classes.cardHeader}
+				/>
+
+				{/* Comments */}
+
+				{comments.map((comment) => (
+					<CardHeader
+						key={comment._id}
+						avatar={
+							<Avatar
+								className={classes.smallAvatar}
+								src={comment.postedBy.avatar}
+							/>
+						}
+						title={this.showComment(comment)}
+						className={classes.cardHeader}
+					/>
+				))}
+			</div>
+		);
 	}
 }
 
